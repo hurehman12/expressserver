009714aad93a24478fa870fe442fb3867ae7111e
hurehman12 <haseeb.rehman@cccareers.org>
2024-07-15 15:04:42 -0400

    Express server project

--- server/app.js
+++ server/app.js
@@ -1,10 +1,33 @@
 // import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
+const data = require('./data.json');
 
+const PORT = process.env.PORT || 3000;
 
 // create your express server below
-var app;
+const app = express();
+
+
 
 // add your routes and middleware below
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+    res.status(200).send('Top Spots Page');
+  });
+  
+  app.get('/data', function(req, res){
+    res.status(200).json(data);
+  });
+
+
+
+
+  // app.listen(PORT, function() {
+  //   // Log a message when the server starts
+  //   console.log(`Server is listening on localhost:${PORT}`);
+  // });
 
 // finally export the express application
 module.exports = app;
